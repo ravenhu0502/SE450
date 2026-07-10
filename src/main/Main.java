5d9cdc2a74142c8b60dce356f7a0330baf7ec543
ravenhu0502 <ravenhu0502@gmail.com>
2026-07-10 16:07:17 -0500

    Add builder package and update Main

--- src/main/Main.java
+++ src/main/Main.java
@@ -17,16 +17,41 @@ public class Main {
 		
 		// jump
 		if(number == 1) {    // student
-			System.out.println("Please enter your first name: ");
-			String name = scanner.next();
-			System.out.println("Hi " + name + ", you are logged in successfully!");
+			Display(scanner);
+			
+			// create a Student object(builder pattern)
+			
+			// student functions
+			System.out.println("\n[1] view all classes\n[2] view my class schedule\n"
+					+ "[3] enroll in classes\n[4] drop classes\n[5] exit\n");
+			System.out.println("Please enter a number: ");
+			int number1 = scanner.nextInt();
+			
 		}
 		
+		if(number == 2) {    // professor
+			Display(scanner);
+			
+			// create a Professor object
+		}
+		
+		if(number == 3) {    // admin
+			Display(scanner);
+			
+			// create an Admin object 
+		}
 		
 		
 		scanner.close();
 	}
 	
+	public static void Display(Scanner scanner) {
+		
+		System.out.println("Please enter your first name: ");
+		String name = scanner.next();
+		System.out.println("Hi " + name + ", you are logged in successfully!");
+		
+	}
 	public int target() {
 		return 1;
 	}
